bb31e5ce05891e370db2ba23f073ec8b88837346
adammetal <adam.metal@codecool.com>
2024-04-10 12:42:26 +0200

    refactor

--- static/script.js
+++ static/script.js
@@ -1,51 +1,69 @@
 const root = document.querySelector("#root");
 const url = "https://api.thecatapi.com/v1/images/search";
 
+let loading = false;
+
+function create(tag, attributes = {}) {
+  const el = document.createElement(tag);
+
+  for (const [key, value] of Object.entries(attributes)) {
+    el[key] = value;
+  }
+
+  return el;
+}
+
 function preload(src) {
   return new Promise((resolve, reject) => {
-    const img = document.createElement("img");
-
-    img.src = src;
+    const img = create("img", { src });
     img.onload = resolve;
     img.onerror = reject;
   });
 }
 
-function getRandomCat() {
-  let src;
+// ??????????????
+let cache = null;
 
-  displayLoading();
-
-  return fetch(url)
-    .then((response) => response.json())
-    .then((catData) => {
-      const cat = catData[0];
-      src = cat.url;
-      return preload(src);
-    })
-    .then(() => {
-      hideLoading();
-      const img = document.createElement("img");
-      img.src = src;
-
-      root.innerHTML = "";
-      root.append(img);
-    });
+async function fetchCat() {
+  const response = await fetch(url);
+  const { url: src } = (await response.json())[0];
+  await preload(src);
+  return src;
 }
 
-function displayLoading() {
-  const div = document.createElement('div');
-  div.id = 'loading';
-  div.innerText = 'Loading...';
-  root.prepend(div);
+async function getRandomCat() {
+  if (loading === true) {
+    return;
+  }
+
+  startLoading();
+  const src = await fetchCat();
+  stopLoading();
+
+  root.innerHTML = "";
+  root.append(create("img", { src }));
 }
 
-function hideLoading() {
-  document.querySelector("#loading").remove();
+function startLoading() {
+  loading = true;
+
+  const el = create("div", {
+    id: "loading",
+    innerText: "loading...",
+  });
+
+  root.prepend(el);
+}
+
+function stopLoading() {
+  loading = false;
+  document.querySelector("#loading")?.remove();
 }
 
 getRandomCat();
 
 root.addEventListener("click", () => {
-  getRandomCat();
+  getRandomCat().catch((err) => {
+    console.log(err);
+  });
 });
